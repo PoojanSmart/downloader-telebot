fb3c37d6528e10cb99a440c89b20b4464579af06
Poojan Smart <44301271+PoojanSmart@users.noreply.github.com>
2023-03-31 08:05:21 +0530

    Action added
    Github action will execute the script

--- poojansbot.py
+++ poojansbot.py
@@ -2,9 +2,20 @@ import telebot
 from telebot import apihelper
 from yt_dlp import YoutubeDL
 import os
+import time
+from threading import Thread
 
 BOT_TOKEN = "6059925047:AAFM40P3XGM74E2jS5-ExGE9W3KjopDABbQ"
 bot = telebot.TeleBot(BOT_TOKEN)
+
+def countdown(bot : telebot.TeleBot):
+    time.sleep(60*4) # runs 4 minutes before stopping the bot
+    bot.stop_bot()
+          
+# Create and launch a thread
+from threading import Thread
+t = Thread(target = countdown, args =(bot, ))
+t.start() 
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
@@ -13,18 +24,19 @@ def send_welcome(message):
 
 @bot.message_handler(regexp='((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$')
 def video_download(message):
-    URLS = [message.text]
-    with YoutubeDL() as ydl:
-        ydl.download(URLS)
-    filename = ""
-    for fname in os.listdir("."):
-        if fname.split(".")[-1] == "mp4":
-            filename = fname
-    bot.send_video( message.chat.id, video=open(filename, 'rb'), supports_streaming=True)
-    os.unlink(filename)
-
+    try:
+        bot.reply_to(message, "Got it, download starting..")
+        URLS = [message.text]
+        with YoutubeDL() as ydl:
+            ydl.download(URLS)
+        bot.reply_to(message, "Download complete, sending..")
+        filename = ""
+        for fname in os.listdir("."):
+            if fname.split(".")[-1] == "mp4":
+                filename = fname
+        bot.send_video( message.chat.id, video=open(filename, 'rb'), supports_streaming=True)
+        bot.reply_to(message, "Complete")
+        os.unlink(filename)
+    except:
+        bot.reply_to(message, f"Sorry, there was some error downloading: {message.text}")
 bot.infinity_polling()
-
-
-
-
